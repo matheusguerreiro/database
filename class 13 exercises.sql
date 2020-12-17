b843c18eee5a0a23513a741db6b78148a75096b7
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2020-12-17 01:06:55 -0300

    Create class 13 exercises.sql

---
+++ class 13 exercises.sql
@@ -0,0 +1,18 @@
+-- Exercícios MySQL Aula 13
+
+-- 1
+select profissao, count(profissao) from gafanhotos
+group by profissao;
+
+-- 2
+select sexo, count(*) from gafanhotos
+where nascimento > '2005-01-01'
+group by sexo;
+
+-- 3
+select nacionalidade, count(*) from gafanhotos
+where nacionalidade <> 'Brasil'
+group by nacionalidade
+having count(nacionalidade) > 3;
+
+-- 4
